cf5d4c47c00187f87736201b5278529e6f08475f
Recep <recep680@outlook.com>
2026-02-05 12:48:35 +0100

    changes from TS to JS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import favicon from '/images/favicon_white.avif';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main>
+	{@render children()}
+</main>
