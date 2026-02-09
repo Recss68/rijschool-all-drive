8cca163e553491b7e9f18be15107dc0b131b57d0
Recep <recep680@outlook.com>
2026-02-09 16:11:17 +0100

    feat: made the benfit component prototype - #8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
-    import { Hero } from '$lib';
+    import { Hero, BenefitCards } from '$lib';
 </script>
 
 <Hero />
+
+<section class="benefits-container section">
+    <div class="section-inner">
+        <h2>Waarom kiezen voor Rijschool All Drive?</h2>
+        <p>Ontdek wat ons uniek maakt en waarom leerlingen voor ons kiezen.</p>
+        <BenefitCards />
+    </div>
+</section>
+
+
