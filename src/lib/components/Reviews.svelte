459cd30f852b139fbc3cf8443a807d990ec5facd
Recep <recep680@outlook.com>
2026-02-13 18:41:28 +0100

    feat: added lazy loading on the reviewer picture

--- src/lib/components/Reviews.svelte
+++ src/lib/components/Reviews.svelte
@@ -29,7 +29,7 @@
         <span class="stars" aria-label="5 out of 5 stars" role="img">★★★★★</span>
         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus pariatur delectus praesentium beatae, aperiam repellat ullam facere ut quibusdam corporis.</p>
         <div class="reviewer-details">
-            <img src="https://placehold.co/50" alt="reviewer-icon">
+            <img src="https://placehold.co/50" alt="reviewer-icon" loading="lazy">
             <div class="reviewer-info">
                 <h3>Naam Achternaam</h3>
                 <p>Geslaagd in 2025</p>
