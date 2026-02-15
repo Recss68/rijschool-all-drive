770453dd9d388f30f76bb43bcaf9400fdf2a7f4d
Recep <recep680@outlook.com>
2026-02-15 20:50:00 +0100

    feat: stories html prototype and json data in images - #16

---
+++ src/lib/components/SuccessStories.svelte
@@ -0,0 +1,15 @@
+<script>
+    import stories from '$lib/data/succes-stories.json';
+</script>
+
+<div class="success-stories">
+    {#each stories as story}
+        <img 
+        src={story.image} 
+        alt="leerling-{story.id}"
+        width="150px"
+        loading="lazy"
+        >
+    {/each}
+</div>
+
