c3a5dc137a4dfd62bfa9d10f4bcf4d0ea53fceef
Recep <recep680@outlook.com>
2026-02-18 23:46:56 +0100

    fix: changed folder structure for clearance

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,13 @@
 // place files you want to import through the `$lib` alias in this folder.
-export { default as Hero } from './components/Hero.svelte';
-export { default as BenefitCards } from './components/BenefitCards.svelte';
-export { default as PriceCards } from './components/PriceCards.svelte';
-export { default as ProcessSteps } from './components/ProcessSteps.svelte';
-export { default as Reviews } from './components/Reviews.svelte';
-export { default as SuccessStories } from './components/SuccessStories.svelte';
-export { default as AboutSection } from './components/AboutSection.svelte';
+
+
+// Layout
+
+// Sections
+export { default as Hero } from './components/sections/Hero.svelte';
+export { default as BenefitCards } from './components/sections/BenefitCards.svelte';
+export { default as PriceCards } from './components/sections/PriceCards.svelte';
+export { default as ProcessSteps } from './components/sections/ProcessSteps.svelte';
+export { default as Reviews } from './components/sections/Reviews.svelte';
+export { default as SuccessStories } from './components/sections/SuccessStories.svelte';
+export { default as AboutSection } from './components/sections/AboutSection.svelte';
