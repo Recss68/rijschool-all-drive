b32265c1380b948d9da436c9d71de993537f6d41
Recep <recep680@outlook.com>
2026-02-19 08:58:05 +0100

    style: made de cta mobile first - #22

--- src/lib/components/sections/CtaSection.svelte
+++ src/lib/components/sections/CtaSection.svelte
@@ -1,8 +1,37 @@
-<section class="cta-section section">
+<section class="cta-section gradient section">
     <div class="section-inner">
         <h2>Klaar om te beginnen?</h2>
         <p>Start vandaag nog met je eerste gratis proefles en ervaar zelf waarom zo veel leerlingen voor ons kiezen.</p>
-        <a class="btn-white" href="https://wa.me/31627824428">Boek je proefles <span class="arrow">→</span></a>
-        <a class="btn-outline" href="tel:+31627824428">Bel direct!</a>
+        <div class="cta-buttons">
+            <a class="btn-white" href="https://wa.me/31627824428">Boek je proefles <span class="arrow">→</span></a>
+            <a class="btn-outline" href="tel:+31627824428">Bel direct!</a>
+        </div>
     </div>
 </section>
+
+<style>
+    .cta-section {
+        text-align: center;
+        background: linear-gradient(to bottom right, var(--g2-0), var(--g2-100));
+        h2 {
+            font-size: var(--fs-hl-lg-mobile);
+        }
+        p {
+            font-size: var(--fs-hl-md-mobile);
+        }
+        h2, p {
+            color: var(--c-white);
+        }
+        .section-inner {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            gap: var(--space-4);
+            .cta-buttons {
+                display: flex;
+                flex-direction: column;
+                gap: var(--space-4);
+            }
+        }
+    }
+</style>
