0184729e062bcf85930cfa00bb7be42a3cd8cc83
Recep <recep680@outlook.com>
2026-02-20 14:52:52 +0100

    feat: made footer HTML prototype - #21

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,3 +1,55 @@
-<footer>
-    
+<footer class="section">
+    <div class="section-inner">
+
+        <div class="footer-heading">
+            <picture>
+                <source srcset="/images/ad_long_white.avif" type="images/avif">
+                <source srcset="/images/ad_long_white.webp" type="images/webp">
+                <img src="/images/ad_long_white.jpg" alt="rijschool-all-drive-logo">
+            </picture>
+            <p>Rijschool All Drive is jouw betrouwbare rijschool in Amsterdam voor het snel en veilig behalen van je rijbewijs. Professioneel, persoonlijk en resultaatgericht – met ervaren instructeurs en een hoog slagingspercentage in Amsterdam.</p>
+        </div>
+
+        <div class="quick-menu-1">
+            <h2>Snelle Links</h2>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Over ons</a></li>
+                <li><a href="/">Prijzen</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="quick-menu-2">
+            <h2>Contact</h2>
+            <ul>
+                <li>+31 6 27824428</li>
+                <li>info@rijschoolalldrive.nl</li>
+                <li>Amsterdam & Omstreken</li>
+            </ul>
+        </div>
+
+        <div class="quick-menu-3">
+            <h2>Openingstijden</h2>
+            <ul>
+                <li>Ma - Vr: 08:00 - 18.:00</li>
+                <li>Za: 09:00 - 17:00</li>
+                <li>Zo: Gesloten</li>
+            </ul>
+            <div class="menu-icons">
+                <ul>
+                    <li>Icon 1</li>
+                    <li>Icon 2</li>
+                    <li>Icon 3</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>© 2026 Rijschool All Drive. Alle rechten voorbehouden.</p>
+            <p>Designed & Hosted by <a class="rec-btn" href="https://devrec.nl">DevRec</a></p>
+        </div>
+
+    </div>
+
 </footer>
