42fcc85c318866a3d08eb430bbabaef8f1366f5a
Recep <recep680@outlook.com>
2026-02-24 15:18:23 +0100

    feat: made contact section and added contact form - #30

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,7 +6,26 @@
 	/>
 </svelte:head>
 
+<script>
+	import { ContactForm } from '$lib';
+</script>
+
 <section class="breadcrumb">
 	<h1>Neem Contact Op</h1>
 	<p>Heb je vragen of wil je direct starten? Wij helpen je graag verder!</p>
+</section>
+
+
+<section class="contact-section section">
+	<div class="section-inner">
+		<div class="contact-grid-left">
+			<h2>Stuur ons een bericht</h2>
+			<p>Vul het formulier in en we nemen binnen 24 uur contact met je op.</p>
+			<ContactForm />
+		</div>
+
+		<div class="contact-grid-right">
+
+		</div>
+	</div>
 </section>
