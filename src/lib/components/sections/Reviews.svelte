ca214aa3a615773c0175cdea0b94cb8f9918fcf4
Recep <recep680@outlook.com>
2026-03-01 21:11:32 +0100

    feat: added the g review embed code - #18

--- src/lib/components/sections/Reviews.svelte
+++ src/lib/components/sections/Reviews.svelte
@@ -1,10 +1,14 @@
 <div class="g-reviews">
+    <script defer async src='https://cdn.trustindex.io/loader.js?ce4c266667ef04213d065dab480'></script>
+</div>
+
+<!-- <div class="g-reviews">
 
     <article class="review">
         <span class="stars" aria-label="5 out of 5 stars" role="img">★★★★★</span>
         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus pariatur delectus praesentium beatae, aperiam repellat ullam facere ut quibusdam corporis.</p>
         <div class="reviewer-details">
-            <img src="https://placehold.co/50" alt="reviewer-icon" loading="lazy" width="50px" height="50px">
+            <img src="https://placehold.co/50" alt="reviewer-icon">
             <div class="reviewer-info">
                 <h3>Naam Achternaam</h3>
                 <p>Geslaagd in 2025</p>
@@ -17,7 +21,7 @@
         <span class="stars" aria-label="5 out of 5 stars" role="img">★★★★★</span>
         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus pariatur delectus praesentium beatae, aperiam repellat ullam facere ut quibusdam corporis.</p>
         <div class="reviewer-details">
-            <img src="https://placehold.co/50" alt="reviewer-icon" loading="lazy" width="50px" height="50px">
+            <img src="https://placehold.co/50" alt="reviewer-icon">
             <div class="reviewer-info">
                 <h3>Naam Achternaam</h3>
                 <p>Geslaagd in 2025</p>
@@ -29,7 +33,7 @@
         <span class="stars" aria-label="5 out of 5 stars" role="img">★★★★★</span>
         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus pariatur delectus praesentium beatae, aperiam repellat ullam facere ut quibusdam corporis.</p>
         <div class="reviewer-details">
-            <img src="https://placehold.co/50" alt="reviewer-icon" loading="lazy" width="50px" height="50px">
+            <img src="https://placehold.co/50" alt="reviewer-icon" loading="lazy">
             <div class="reviewer-info">
                 <h3>Naam Achternaam</h3>
                 <p>Geslaagd in 2025</p>
@@ -82,4 +86,4 @@
             }
         }
     }
-</style>
+</style> -->
