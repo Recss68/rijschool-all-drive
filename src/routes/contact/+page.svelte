add77f4384484a8725fdf552378af972c9ed81ce
Recep <recep680@outlook.com>
2026-03-03 23:02:40 +0100

    feat: made service area cards html prototype - #38

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -94,7 +94,44 @@
 	</div>
 </section>
 
+<section class="section service-area">
+	<div class="section-inner">
+		<h2>Ons werkgebied</h2>
+		<p>Wij verzorgen rijlessen in Amsterdam en directe omgeving</p>
+		<div class="service-area-cards">
+
+			<article class="area-card">
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+				<h3>Amsterdam</h3>
+				<p>Rijlessen in Amsterdam West, Rijlessen in Amsterdam Noord, Rijlessen in Amsterdam Zuid en Rijlessen in Amsterdam Zuid.</p>
+			</article>
+
+			<article class="area-card">
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+				<h3>Amsterdam Centrum</h3>
+				<p>Ook in Amsterdam Centrum zijn wij actief met onze rijlessen.</p>
+			</article>
+
+			<article class="area-card">
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#155DFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+				<h3>Omliggende gebieden</h3>
+				<p>Rijlessen Diemen, Rijlessen Haarlem, Rijlessen Amstelveen en omstreken.</p>
+			</article>
+
+		</div>
+		<p>Niet zeker of we in jouw buurt rijden? Neem contact met ons op voor meer informatie!</p>
+	</div>
+</section>
+
 <style>
+
+    .service-area { 
+        background-color: var(--c-section);
+        text-align: center;
+        h2 {
+            font-size: var(--fs-hl-lg);
+        }
+    }
 
 	.contact-section {
 		.section-inner {
@@ -166,4 +203,5 @@
 		color: var(--c-text-muted);
 		font-size: var(--fs-label-sm);
 	}
+
 </style>
