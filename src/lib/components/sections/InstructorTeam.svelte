a2b53ea6dc4120db5b68de0ee8b319b855f3f2e1
Recep <recep680@outlook.com>
2026-03-08 23:22:02 +0100

    style: added instructors team styling - #44

--- src/lib/components/sections/InstructorTeam.svelte
+++ src/lib/components/sections/InstructorTeam.svelte
@@ -12,3 +12,27 @@
         </div>
     {/each}
 </div>
+
+<style>
+    .instructor-team {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        .instructor {
+            background-color: var(--c-white);
+            width: 500px;
+            padding: var(--space-6);
+            border-radius: var(--radius-m);
+            span {
+                background-color: var(--c-accent);
+                padding: var(--space-6);
+                display: inline-block;
+                color: var(--c-white);
+                font-weight: var(--fw-bold);
+                border-radius: var(--radius-m);
+                margin-bottom: var(--space-6);
+            }
+        }
+    }
+</style>
